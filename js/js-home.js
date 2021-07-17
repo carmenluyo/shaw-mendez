aaf775138a256ae96bf6d434f08b8d67fa26d5f6
Carmen Luyo <c.luyorosales@gmail.com>
2021-07-17 08:37:28 -0500

    se actualizo css y html

--- js/js-home.js
+++ js/js-home.js
@@ -1,4 +1,29 @@
 jQuery(document).ready(function($) {
+  $('.slider-home').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [{
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+           breakpoint: 400,
+           settings: {
+              arrows: false,
+              slidesToShow: 1,
+              slidesToScroll: 1
+           }
+        }]
+    });
+
       $('.slider').slick({
         dots: false,
         infinite: true,
